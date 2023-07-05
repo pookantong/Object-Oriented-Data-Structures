ab8fd5d889910ecf78ab6abf21e08e59d884638d
Yotsapat Punyaworapan <110213795+pookantong@users.noreply.github.com>
2023-07-05 16:03:21 +0700

    refactor: code condition to class

--- OOD_W2/1_5.py
+++ OOD_W2/1_5.py
@@ -1,25 +1,31 @@
 print('*** TorKham HanSaa ***')
 
+class ThorKham:
+    def __init__(self) -> None:
+        self.khamList = []
+
+    def playGame(self, orders):
+        for order in orders:
+            orderSplit = order.split()
+            if orderSplit[0] == 'P':
+                kham = orderSplit[1]
+                if len(self.khamList) == 0:
+                    self.khamList.append(kham)
+                    print(f'\'{kham}\' -> {self.khamList}')
+                elif kham[:2].lower() == self.khamList[-1][-2:].lower():
+                    self.khamList.append(kham)
+                    print(f'\'{kham}\' -> {self.khamList}')
+                else:
+                    print(f'\'{kham}\' -> game over')
+                    
+            elif orderSplit[0] == 'R':
+                print('game restarted')
+                self.khamList = []
+            elif orderSplit[0] == 'X':
+                break
+            else:
+                print(f'\'{order}\' is Invalid Input !!!')
+                break
+
 orders = input('Enter Input : ').split(',')
-khamList = []
-for order in orders:
-    orderSplit = order.split()
-    if orderSplit[0] == 'P':
-        kham = orderSplit[1]
-        if len(khamList) == 0:
-            khamList.append(kham)
-            print(f'\'{kham}\' -> {khamList}')
-        elif kham[:2].lower() == khamList[-1][-2:].lower():
-            khamList.append(kham)
-            print(f'\'{kham}\' -> {khamList}')
-        else:
-            print(f'\'{kham}\' -> game over')
-            
-    elif orderSplit[0] == 'R':
-        print('game restarted')
-        khamList = []
-    elif orderSplit[0] == 'X':
-        break
-    else:
-        print(f'\'{order}\' is Invalid Input !!!')
-        break
+ThorKham().playGame(orders)
