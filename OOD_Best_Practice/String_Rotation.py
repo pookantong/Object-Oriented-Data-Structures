1d0e941a4c09b8155af790aef313dd96bdb726a4
Yotsapat Punyaworapan <110213795+pookantong@users.noreply.github.com>
2023-07-06 01:45:50 +0700

    refactor: rename

--- OOD_Best_Practice/String_Rotation.py
+++ OOD_Best_Practice/String_Rotation.py
@@ -1,13 +1,13 @@
 print('*** String Rotation ***')
 str1, str2 = input('Enter 2 strings : ').split()
-loopStr1 = str1[-2:] + str1[0:-2]
-loopStr2 = str2[3:] + str2[0:3]
+loopStr1 = str1[-2:] + str1[:-2]
+loopStr2 = str2[3:] + str2[:3]
 count = 1
 while loopStr1 != str1 or loopStr2 != str2:
     if count <= 5:
         print(count, loopStr1, loopStr2)
-    loopStr1 = loopStr1[-2:] + loopStr1[0:-2]
-    loopStr2 = loopStr2[3:] + loopStr2[0:3]
+    loopStr1 = loopStr1[-2:] + loopStr1[:-2]
+    loopStr2 = loopStr2[3:] + loopStr2[:3]
     count+=1
 if count > 5:
     print(' . . . . . ')
