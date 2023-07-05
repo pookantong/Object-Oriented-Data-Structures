d197d97ddd1187813de471a30a69a3904aa33548
Yotsapat Punyaworapan <110213795+pookantong@users.noreply.github.com>
2023-07-05 15:56:44 +0700

    refactor: code W_2 1_4

--- OOD_W2/1_4.py
+++ OOD_W2/1_4.py
@@ -1,17 +1,19 @@
-def sumEqualFive(numList):
-    if len(numList) < 3:
-        return 'Array Input Length Must More Than 2'
-    result = []
-    for i in range(len(numList)):
-        for j in range(i+1, len(numList)):
-            for k in range(j+1, len(numList)):
-                fiveList = [numList[i], numList[j], numList[k]]
-                fiveList.sort()
-                if numList[i] + numList[j] + numList[k] == 5 and set(fiveList) not in [set(x) for x in result]:
-                    result.append(fiveList)
-    return result
-
+def sumEqualFive(numList, tripNum, result):
+    if len(tripNum) == 3 and sum(tripNum) == 5 and set(tripNum) not in [set(x) for x in result]:
+        tripNum.sort()
+        result.append(tripNum)
+        return
+    elif len(tripNum) == 3 or not numList:
+        return
+    
+    sumEqualFive(numList[1:], tripNum + [numList[0]], result)
+    sumEqualFive(numList[1:], tripNum, result)
 
 numList = [int(x) for x in input('Enter Your List : ').split()]
-
-print(sumEqualFive(numList))
+result = []
+tripNum = []
+if len(numList) < 3:
+        print('Array Input Length Must More Than 2')
+else:
+    sumEqualFive(numList, tripNum, result)
+    print(result)
