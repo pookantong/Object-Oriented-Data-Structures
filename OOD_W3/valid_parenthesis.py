0303db602e2f8cf7120ea6877f6dfd87aaac7040
Yotsapat Punyaworapan <110213795+pookantong@users.noreply.github.com>
2023-07-11 15:49:44 +0700

    fix: stack class in valid_parenthesis

--- OOD_W3/valid_parenthesis.py
+++ OOD_W3/valid_parenthesis.py
@@ -1,3 +1,21 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+        
+    def push(self, item):
+        self.items.append(item)
+    
+    def pop(self):
+        res = self.items[-1]
+        self.items = self.items[:-1]
+        return res
+    
+    def isEmpty(self):
+        return self.size() == 0
+    
+    def size(self):
+        return len(self.items)
+
 class Parenthesis:
     def __init__(self):
         self.par = {
@@ -8,17 +26,17 @@ class Parenthesis:
         self.all_par = ['[','{','(',')','}',']']
 
     def valid_parenthesis(self, pars):
-        stack = []
+        stack = Stack()
         for char in pars:
             if char in self.all_par:
                 if char in self.par:
-                    stack.append(char)
-                elif stack == []:
+                    stack.push(char)
+                elif stack.isEmpty():
                     return print(f'{input} close paren excess')
                 elif self.par[stack.pop()] != char:
                     return print(f'{input} Unmatch open-close  ')
-        if stack != []:
-            return print(f'{input} open paren excess   {len(stack)} : ', *stack, sep='')
+        if not stack.isEmpty:
+            return print(f'{input} open paren excess   {len(stack.items)} : ', *stack.items, sep='')
         return print(f'{input} MATCH')
 
 
